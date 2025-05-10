bef53bf291c85ea1a1fb6684ff9da92e388aa820
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-05-10 18:59:49 +0300

    Use the case-insensitive operator -ieq for string comparison

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$result = Get-AzDisk | Where-Object {$_.DiskState -eq "Unattached" -and $_.ResourceGroupName -eq "mate-azure-task-5"}
+$result = Get-AzDisk | Where-Object {$_.DiskState -eq "Unattached" -and $_.ResourceGroupName -ieq "mate-azure-task-5"}
 
 $result | Select-Object Name, DiskState, ResourceGroupName | ConvertTo-Json | Set-Content -Path "result.json"
 
