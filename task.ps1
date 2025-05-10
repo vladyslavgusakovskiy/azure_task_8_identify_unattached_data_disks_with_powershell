44c2220a62bfaf35343c737bab35cada18922e1f
Vladyslav Gusakovskiy <vladyslav.gusakovskiy@gmail.com>
2025-05-10 18:46:32 +0300

    Add script to identify unattached disks

--- task.ps1
+++ task.ps1
@@ -1,1 +1,4 @@
-# Write your code here
+$result = Get-AzDisk | Where-Object {$_.DiskState -eq "Unattached" -and $_.ResourceGroupName -eq "mate-azure-task-5"}
+
+$result | Select-Object Name, DiskState, ResourceGroupName | ConvertTo-Json | Set-Content -Path "result.json"
+
